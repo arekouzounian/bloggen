8016f56db3e3bf341fe73f0bdfb3cbf6fc994684
Arek Ouzounian <agouzo777@gmail.com>
2024-06-08 17:33:40 -0700

    frontend updates for metadata

--- bloggen-frontend/app/posts/page.js
+++ bloggen-frontend/app/posts/page.js
@@ -9,13 +9,45 @@ export default function Page() {
     const p = path.resolve(process.cwd(), 'app', 'static'); 
     const posts = fs.readdirSync(p);
 
-    // TODO: Populate metadata and sort by created
+    const stats = new Map(); 
+    posts.forEach((post, _) => {
+        stats.set(post, fs.statSync(path.join(p, post)));
+
+        let a = fs.statSync(p);
+
+        a.ctime
+    }); 
+
+    posts.sort((a, b) => 
+        stats.get(a).ctime - stats.get(b).ctime 
+    )
+
+    const descriptions = new Map(); 
+    posts.forEach((post, _) => {
+        let meta_path = path.join(p, post, "meta.json")
+        let desc = "no description provided";
+
+        if (fs.existsSync(meta_path)) {
+            let meta = fs.readFileSync(meta_path).toString();
+            let js = JSON.parse(meta);
+            desc = js.Description != null ? js.Description : desc; 
+        }
+
+        descriptions.set(post, desc);
+    })
+    
 
     return (
         <div className="text-center">
             <h1 className="text-3xl bold m-5"> Blog Posts </h1>
-            <div className="grid gap-4 grid-cols-1 bold underline ">
-                {posts.map((post, i) => <div key={i} className="box-content border-2 rounded-md p-3"><a href={"./posts/" + post}>{post}</a></div>)}
+            <div className="grid gap-4 grid-cols-1">
+                {posts.map((post, i) => 
+                    <a key={i} className="box-content border-2 rounded-md p-3 hover:animate-pulse" href={"./posts/" + post}>
+                        <div>
+                            <p className='bold underline'>{post}</p>
+                            <p className="italic opacity-70">{descriptions.get(post)}</p>
+                        </div>
+                    </a>)}
             </div>
             <Footer />
         </div>
